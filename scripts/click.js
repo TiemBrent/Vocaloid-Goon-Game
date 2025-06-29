99730da8106e44a954505f8ceb40108f0b7ba814
TiemBrent <brentlankhaar2258@gmail.com>
2025-06-29 20:17:51 +0200

    Delete scripts directory

--- scripts/click.js
+++
@@ -1,9 +0,0 @@
-
-// Click to earn coins
-document.addEventListener('DOMContentLoaded', () => {
-    const img = document.getElementById('clicker');
-    if (!img) return;
-    img.onclick = () => {
-        addCoins(1);
-    };
-});
--- scripts/images.js
+++
@@ -1,5 +0,0 @@
-const images = [];
-for (let i = 1; i <= 80; i++) images.push(`goon${i}.png`);
-for (let i = 1; i <= 482; i++) images.push(`goon${i}.jpg`);
-for (let i = 1; i <= 230; i++) images.push(`goon${i}.jpeg`);
-for (let i = 1; i <= 31; i++) images.push(`goon${i}.webp`);
